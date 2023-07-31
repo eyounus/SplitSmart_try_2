12a7b155af86e811fbb0f68d6cb8300513313ab7
elishbah <eyounus@umich.edu>
2023-07-31 05:28:48 -0400

    login redirect

--- app/src/main/java/com/example/myapplication2/LoginActivity.java
+++ app/src/main/java/com/example/myapplication2/LoginActivity.java
@@ -2,7 +2,10 @@ package com.example.myapplication2;
 
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.content.Intent;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
 
 public class LoginActivity extends AppCompatActivity {
 
@@ -10,5 +13,16 @@ public class LoginActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_login);
+
+        Button btn = (Button)findViewById(R.id.login_button);
+        btn.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                startActivity(new Intent(LoginActivity.this , MainActivity.class));
+            }
+        });
     }
+
+
+
 }
